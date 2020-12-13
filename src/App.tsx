4efe4e2652c2785361b25857b5419663260a9e15
KristapsN <kristaps.nogobods@gmail.com>
2020-12-13 14:15:40 +0200

    readme update

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,9 @@ const App = () => {
   ]);
   const [snakeApples, setSnakeApples] = useState([
     { id: 0, xApple: 12, yApple: 0, eaten: false },
+    { id: 0, xApple: 14, yApple: 0, eaten: false },
+    { id: 0, xApple: 16, yApple: 0, eaten: false },
+    { id: 0, xApple: 18, yApple: 0, eaten: false },
     { id: 1, xApple: 22, yApple: 56, eaten: false },
     { id: 2, xApple: 16, yApple: 16, eaten: false },
     { id: 3, xApple: 32, yApple: 78, eaten: false },
@@ -44,7 +47,7 @@ const App = () => {
         snakeHead[snakeStart] = [snakeHead[snakeStart][0] + 0,
           snakeHead[snakeStart][1] - 2];
         break;
-      case  'ArrowDown':
+      case 'ArrowDown':
         snakeHead[snakeStart] = [snakeHead[snakeStart][0] + 0,
           snakeHead[snakeStart][1] + 2];
         break;
@@ -53,7 +56,8 @@ const App = () => {
     dots.shift();
     setSnakeHead(dots);
     snakeApples.map(({ xApple, yApple }, index) => {
-      if (snakeHead[snakeStart][0] === xApple && snakeHead[snakeStart][1] === yApple) {
+      if (snakeHead[snakeStart][0] === xApple &&
+         snakeHead[snakeStart][1] === yApple) {
         dots.push(snakeHead[snakeStart]);
         snakeApples[index].eaten = !snakeApples[index].eaten;
         setSnakeApples(snakeApples);
@@ -68,24 +72,28 @@ const App = () => {
         setIsActive(false);
         setResult('You Lose!');
       }
-      if (snakeApples.length === 0){
+      if (snakeApples.length === 0) {
         setIsActive(false);
         setResult('You Won!');
       }
       // if (snakeHead[snakeStart][0] === snakeHead[snakeStart-1][0] && snakeHead[snakeStart][1] === snakeHead[snakeStart-1][1]){
-        
+
       // }
-      
-     
+      // if (dots[snakeStart]=== dots[snakeStart-1].find(item => dots[snakeStart] )){
+      //   console.log('Crash');
+      // }
+      console.log(dots[snakeStart]);
+
+
     });
-  }; 
+  };
 
   const reset = () => {
     setSeconds(0);
     setIsActive(!isActive);
   };
- 
-  let setId: string; 
+
+  let setId: string;
   useEffect(() => {
     setId = uuidv4();
     let interval = 0;
@@ -105,28 +113,33 @@ const App = () => {
 
 
   return (
-    <div>
-      {/* <h2>{seconds}</h2> */}
-      <h3>{result}</h3>
-      <button type='button' onClick={() => reset()}>Pause</button>
-      <div className="game--area">
-        {snakeHead.map((item, index) =>
-          <div key={setId}>
-            <SnakeHead
-              x={item[0]}
-              y={item[1]}
-            />
+    <div className='container'>
+      <div className="row center-xs">
+        <div className="col-xs-6">
+          <h2>{seconds/10}</h2>
+          <h3>{result}</h3>
+
+          <button type='button' onClick={() => reset()}>Pause</button>
+          <div className="game--area">
+            {snakeHead.map((item, index) =>
+              <div key={setId}>
+                <SnakeHead
+                  x={item[0]}
+                  y={item[1]}
+                />
+              </div>
+            )}
+            {snakeApples.map(({ id, xApple, yApple, eaten }) =>
+              <div key={setId}>
+                <SnakeApple
+                  xApple={xApple}
+                  yApple={yApple}
+                  eaten={eaten}
+                />
+              </div>
+            )}
           </div>
-        )}
-        {snakeApples.map(({ id, xApple, yApple, eaten }) =>
-          <div key={id}>
-            <SnakeApple
-              xApple={xApple}
-              yApple={yApple}
-              eaten={eaten}
-            />
-          </div>
-        )}
+        </div>
       </div>
     </div>
   );
